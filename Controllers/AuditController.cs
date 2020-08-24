7a8140165cae932b0df026b5adc923260a52d120
dotnetcloudmc18 <dotnetcloudmc18@CTSDCLOUDMC25>
2020-08-24 23:43:12 +0530

    Mapping

---
+++ Controllers/AuditController.cs
@@ -0,0 +1,49 @@
+﻿using MC18_S1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MC18_S1.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/[controller]")]
+    public class AuditController : ControllerBase
+    {
+        private readonly ILogger<AuditController> _logger;
+        private IDataAccess<AuditorUser> ctx;
+        //public AuditController()
+        //{
+        //    this.ctx = new DataAccessRepository<Auditors>();
+        //}
+        public AuditController(ILogger<AuditController> logger, IDataAccess<AuditorUser> dataAccess)
+        {
+            _logger = logger;
+            ctx = dataAccess;
+        }
+
+        [HttpGet]
+        public IEnumerable<AuditorUser> GetAuditors()
+        {
+            return ctx.GetAll();
+        }
+        [HttpPost]
+        public void PostAuditors(AuditorUser a)
+        {
+            ctx.Insert(a);
+        }
+        [HttpPut]
+        public void updateAuditors(AuditorUser b)
+        {
+            ctx.Update(b);
+        }
+        [HttpDelete]
+        public void DeleteAuditor(int Id)
+        {
+            ctx.Delete(Id);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MC18_S1.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,14 +27,38 @@ namespace MC18_S1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddControllers();
+            services.AddApiVersioning(x =>
+            {
+                x.DefaultApiVersion = new ApiVersion(1, 0);
+                x.AssumeDefaultVersionWhenUnspecified = true;
+                x.ReportApiVersions = true;
+            });
+            services.AddDbContext<Models.AuditorUserContext>(opts =>
+            opts.UseSqlServer("AuditorUser"));
+            services.AddTransient(typeof(IDataAccess<AuditorUser>), typeof(DataAccessRepository<AuditorUser>));
+            services.AddTransient(typeof(IDataAccess<Portfolio>), typeof(DataAccessRepository<Portfolio>));
+            services.AddTransient(typeof(IDataAccess<RequestAudit>), typeof(DataAccessRepository<RequestAudit>));
+            services.AddMvc();
+            
+
             services.AddDbContext<Models.AuditorUserContext>(opt =>
                opt.UseInMemoryDatabase("AuditorUser"));
             services.AddControllers();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger(c =>
+            {
+                c.SerializeAsV2 = true;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -45,10 +70,18 @@ namespace MC18_S1
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
             {
-                endpoints.MapControllers();
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Auditor Service");
             });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    //endpoints.MapControllers();
+            //    endpoints.MapControllerRoute("default", "{controller=Audit}/{action=GetAuditors}/{id?}");
+              
+            //});
         }
     }
 }
